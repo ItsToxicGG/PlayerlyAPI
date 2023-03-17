ed6206537b8aa39bd7d9d3ef745d779bae4e03f8
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2023-03-17 09:04:02 -0700

    Update SessionTimeTask.php

--- src/Statics/task/SessionTimeTask.php
+++ src/Statics/task/SessionTimeTask.php
@@ -4,6 +4,7 @@ namespace Statics\task;
 
 use pocketmine\scheduler\Task;
 use pocketmine\player\Player;
+use pocketmine\Server;
 
 class SessionTimeTask extends Task {
 
@@ -14,17 +15,17 @@ public function __construct(\mysqli $db) {
     $this->db = $db;
 }
 
-public function onRun(int $currentTick) {
+public function onRun() : void {
     foreach (Server::getInstance()->getOnlinePlayers() as $player) {
         $username = strtolower($player->getName());
         $sessionTime = $this->getSessionTime($username);
-        $this->database->query("UPDATE stats SET time = $sessionTime WHERE username = '$username'");
+        $this->db->query("UPDATE stats SET time = $sessionTime WHERE username = '$username'");
     }
 }
 
 public function getSessionTime($username) {
     $username = $player->getDisplayName();
-    $result = $this->database->query("SELECT time FROM stats WHERE username = '$username'");
+    $result = $this->db->query("SELECT time FROM stats WHERE username = '$username'");
     if ($result instanceof \mysqli_result && $result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $sessionTime = $row["time"] + 1;

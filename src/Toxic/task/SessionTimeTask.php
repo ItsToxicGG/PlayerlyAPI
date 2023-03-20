bd10089d6ac37cd2d5b9f47b80e4dc7e6e0d851c
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2023-03-20 13:49:12 -0700

    attempt to not register each player at time

--- src/Toxic/task/SessionTimeTask.php
+++ src/Toxic/task/SessionTimeTask.php
@@ -30,22 +30,24 @@ private $db;
 public function __construct(\mysqli $db) {
     $this->db = $db;
 }
+    public function onRun() : void {
+        $players = Server::getInstance()->getOnlinePlayers();
+        $numPlayers = count($players);
 
-public function onRun() : void {
-    foreach (Server::getInstance()->getOnlinePlayers() as $player) {
-        $username = strtolower($player->getName());
-        $sessionTime = $this->getSessionTime($username);
-        $this->db->query("UPDATE stats SET time = $sessionTime WHERE username = '$username'");
-    }
-}
+        // Batch the queries and updates
+        $batchedQueries = [];
+        for ($i = 0; $i < $numPlayers; $i += $this->batchSize) {
+            $batch = array_slice($players, $i, $this->batchSize);
+            $usernames = array_map(function($player) {
+                return strtolower($player->getName());
+            }, $batch);
+            $usernameList = "'" . implode("','", $usernames) . "'";
+            $batchedQueries[] = "UPDATE stats SET time = time + 1 WHERE username IN ($usernameList)";
+        }
 
-public function getSessionTime($username) {
-    $result = $this->db->query("SELECT time FROM stats WHERE username = '$username'");
-    $sessionTime = 0;
-    if ($result instanceof \mysqli_result && $result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $sessionTime = (int) $row["time"] + 1;
+        // Execute the batched queries
+        foreach ($batchedQueries as $query) {
+            $this->db->query($query);
+        }
     }
-    return $sessionTime;
-}
 }
